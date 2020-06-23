b8fa1ad9c2763debe962de846386ff3641f00f3e
dangreen <danon0404@gmail.com>
2020-06-23 23:31:37 +0700

    fix build package

--- scripts/buildPackage.sh
+++ scripts/buildPackage.sh
@@ -5,6 +5,8 @@ set -e
 trigen-scripts build
 rm -rf package
 mv lib package
+cp src/runtime.d.ts package
+cp src/types.d.ts package
 cp LICENSE package
 cp package.json package
 cp README.md package
