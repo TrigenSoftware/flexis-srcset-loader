d34463f80978c249ebdd427460807a1dffcb29b7
Dan Onoshko <danon0404@gmail.com>
2021-02-01 18:35:20 +0700

    Enhancement/depds upgrade (#250)
    * universal test
    
    * srcset upgrade
    
    * mime type and other mods

--- test/buildTestBundle.js
+++ test/buildTestBundle.js
@@ -6,7 +6,12 @@ compile('image.js', {
 	rules: [{
 		match: '(max-width: 3000px)',
 		width: [1, .5, 1920, 1280, 720, 560, 320],
-		format: ['webp', 'jpg']
+		format: ['avif', 'webp', 'jpg']
 	}],
-	scalingUp: false
+	scalingUp: false,
+	processing: {
+		avif: {
+			quality: 60
+		}
+	}
 }, true);
