91e164852e95b713e3655e98b91dd51ad83acdc6
dangreen <danon0404@gmail.com>
2021-02-13 01:49:22 +0700

    test mods

--- test/loader.spec.js
+++ test/loader.spec.js
@@ -1,4 +1,6 @@
-import compile, {
+import {
+	compile,
+	multiCompile,
 	fs,
 	pathToArtifacts
 } from './compile';
@@ -50,5 +52,55 @@ describe('srcset-loader', () => {
 			'$1asset$2'
 		)).toMatchSnapshot();
 		expect(artifacts.length).toBe(8);
+	});
+
+	it('should use cache with runtimeModulesCache option', async () => {
+		// Warm up
+		await multiCompile('image.js', {
+			rules: [{
+				match: '(max-width: 3000px)',
+				width: [1, 1920, 1280, 720, 560, 320],
+				format: ['webp', 'jpg']
+			}],
+			scalingUp: false,
+			processOnce: false
+		});
+		await multiCompile('image.js', {
+			rules: [{
+				match: '(max-width: 3000px)',
+				width: [1, 1920, 1280, 720, 560, 320],
+				format: ['webp', 'jpg']
+			}],
+			scalingUp: false,
+			processOnce: true
+		});
+
+		let stats = null;
+
+		stats = await multiCompile('image.js', {
+			rules: [{
+				match: '(max-width: 3000px)',
+				width: [1, 1920, 1280, 720, 560, 320],
+				format: ['webp', 'jpg']
+			}],
+			scalingUp: false,
+			processOnce: true
+		});
+
+		const timeWithCache = stats.children.reduce((sum, stats) => sum + stats.time, 0);
+
+		stats = await multiCompile('image.js', {
+			rules: [{
+				match: '(max-width: 3000px)',
+				width: [1, 1920, 1280, 720, 560, 320],
+				format: ['webp', 'jpg']
+			}],
+			scalingUp: false,
+			processOnce: false
+		});
+
+		const timeWithoutCache = stats.children.reduce((sum, stats) => sum + stats.time, 0);
+
+		expect(timeWithoutCache).toBeGreaterThan(timeWithCache);
 	});
 });
